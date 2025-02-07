fa93985816171da8b87dec9ca252a8a1d1bf438b
Max <55560490+maks1302@users.noreply.github.com>
2025-02-07 11:33:56 +0000

    Restored to '0e8316b34d32784ba1c7b3f31ae21d61c2341501'
    Replit-Restored-To:0e8316b34d32784ba1c7b3f31ae21d61c2341501

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -12,7 +12,6 @@ export default function Home() {
     search_terms: string;
     ad_type: string;
     country: string[];
-    languages?: string[];
     ad_active_status: string;
     ad_delivery_date_min?: string;
     ad_delivery_date_max?: string;
